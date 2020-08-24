41c76dee9037071c809a0f2754b12a1dbbc478e3
Ric Evans <emejqz@gmail.com>
2020-08-24 16:29:48 -0500

    add argparse

--- web_app/__main__.py
+++ web_app/__main__.py
@@ -1,10 +1,21 @@
 """Root python script for MoU Dashboard web application."""
 
+import argparse
 import logging
 
 # local imports
 from web_app.config import app
 
 if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(
+        description="Find files under PATH(s), compute their metadata and "
+        "upload it to File Catalog.",
+        epilog="Notes: (1) symbolic links are never followed.",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument("-p", "--port", type=int, default=8050, help="port to bind")
+    args = parser.parse_args()
+
     logging.getLogger().setLevel(logging.INFO)
-    app.run_server(debug=True)
+    app.run_server(debug=True, host="localhost", port=args.port)
