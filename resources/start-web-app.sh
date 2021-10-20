eb0115521604c6a4c7bba352c2ba40babd5d881d
Ric Evans <emejqz@gmail.com>
2021-10-19 19:27:08 -0500

    use 4318

--- resources/start-web-app.sh
+++ resources/start-web-app.sh
@@ -1,6 +1,6 @@
 # start up the REST server with Tracing
 
 export REST_SERVER_URL=http://localhost:8079
-export OTEL_EXPORTER_OTLP_ENDPOINT="http://localhost:4317/v1/traces"
+export OTEL_EXPORTER_OTLP_ENDPOINT="http://localhost:4318/v1/traces"
 
 python -m web_app
