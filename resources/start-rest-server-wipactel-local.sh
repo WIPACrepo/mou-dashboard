f72d88dd1ff14ef566ecd342ed2007615042bac2
Ric Evans <emejqz@gmail.com>
2021-11-02 10:33:50 -0500

    add prefix to startup scripts

--- resources/start-rest-server-wipactel-local.sh
+++ resources/start-rest-server-wipactel-local.sh
@@ -1,6 +1,7 @@
 # start up the REST server with Tracing
 
-export REST_SERVER_URL=http://localhost:8079
+export MOU_REST_PORT=8079
 export OTEL_EXPORTER_OTLP_ENDPOINT="http://localhost:4318/v1/traces"
+export WIPACTEL_SERVICE_NAME_PREFIX=mou
 
-python -m web_app
+python -m rest_server
