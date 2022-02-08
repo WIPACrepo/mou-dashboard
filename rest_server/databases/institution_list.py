791c89e677998d6e34b47c75c82e809b7739c48e
Ric Evans <emejqz@gmail.com>
2022-02-08 17:59:39 -0600

    add `institution_list.py` (temp)

---
+++ rest_server/databases/institution_list.py
@@ -0,0 +1,162 @@
+"""The master list of IceCube institutions and their details.
+
+More or less,
+ a more structured JSON file (or less structured MongoDB)
+
+NOTE: Keep things simple here. Avoid non-basic imports,
+
+      so file can be imported by 3rd parties independently.
+"""
+
+INSTITUTIONS = {
+    "Alabama": {
+        "has_mou": True,
+        "name": "University of Alabama",
+        "is_US": True,
+    },
+    "Alaska-Anchorage": {
+        "has_mou": True,
+        "name": "University of Alaska Anchorage",
+        "is_US": True,
+    },
+    "Clark-Atlanta": {
+        "has_mou": True,
+        "name": "Clark-Atlanta University",
+        "is_US": True,
+    },
+    "Delaware": {
+        "has_mou": True,
+        "name": "University of Delaware",
+        "is_US": True,
+    },
+    "Drexel": {
+        "has_mou": True,
+        "name": "Drexel University",
+        "is_US": True,
+    },
+    "GaTech": {
+        "has_mou": True,
+        "name": "Georgia Institute of Technology",
+        "is_US": True,
+    },
+    "Harvard": {
+        "has_mou": True,
+        "name": "Harvard University",
+        "is_US": True,
+    },
+    "Kansas": {
+        "has_mou": True,
+        "name": "University of Kansas",
+        "is_US": True,
+    },
+    "LBNL": {
+        "has_mou": True,
+        "name": "Lawrence Berkeley National Laboratory",
+        "is_US": True,
+    },
+    "Loyola": {
+        "has_mou": True,
+        "name": "Loyola University Chicago",
+        "is_US": True,
+    },
+    "MIT": {
+        "has_mou": False,
+        "name": "Massachusetts Institute of Technology",
+        "is_US": True,
+    },
+    "Marquette": {
+        "has_mou": True,
+        "name": "Marquette University",
+        "is_US": True,
+    },
+    "Maryland": {
+        "has_mou": True,
+        "name": "University of Maryland",
+        "is_US": True,
+    },
+    "Mercer": {
+        "has_mou": True,
+        "name": "Mercer University",
+        "is_US": True,
+    },
+    "Michigan-State": {
+        "has_mou": True,
+        "name": "Michigan State University",
+        "is_US": True,
+    },
+    "Ohio-State": {
+        "has_mou": True,
+        "name": "Ohio State University",
+        "is_US": True,
+    },
+    "Penn-State": {
+        "has_mou": True,
+        "name": "Pennsylvania State University",
+        "is_US": True,
+    },
+    "Rochester": {
+        "has_mou": True,
+        "name": "University of Rochester",
+        "is_US": True,
+    },
+    "SD-Mines-Tech": {
+        "has_mou": True,
+        "name": "South Dakota School of Mines and Technology",
+        "is_US": True,
+    },
+    "Southern": {
+        "has_mou": True,
+        "name": "Southern University",
+        "is_US": True,
+    },
+    "Stony-Brook": {
+        "has_mou": True,
+        "name": "Stony Brook University",
+        "is_US": True,
+    },
+    "Texas-Arlington": {
+        "has_mou": True,
+        "name": "University of Texas at Arlington",
+        "is_US": True,
+    },
+    "UC-Berkeley": {
+        "has_mou": False,
+        "name": "University of California, Berkeley",
+        "is_US": True,
+    },
+    "UC-Irvine": {
+        "has_mou": True,
+        "name": "University of California, Irvine",
+        "is_US": True,
+    },
+    "UCLA": {
+        "has_mou": True,
+        "name": "UCLA",
+        "is_US": True,
+    },
+    "UW-Madison": {
+        "has_mou": True,
+        "name": "University of Wisconsin–Madison",
+        "is_US": True,
+    },
+    "UW-River-Falls": {
+        "has_mou": True,
+        "name": "University of Wisconsin, River Falls",
+        "is_US": True,
+    },
+    "Utah": {
+        "has_mou": True,
+        "name": "University of Utah",
+        "is_US": True,
+    },
+    "Washington": {
+        "has_mou": False,
+        "name": "University of Washington",
+        "is_US": True,
+    },
+    "Yale": {
+        "has_mou": True,
+        "name": "Yale University",
+        "is_US": True,
+    },
+}
