e31c0a2dfc82403b468f833e723ea12e848f49ea
Ric Evans <emejqz@gmail.com>
2022-02-22 14:56:00 -0600

    add caching to krs-institution fetching

--- web_app/data_source/institution_info.py
+++ web_app/data_source/institution_info.py
@@ -1,8 +1,11 @@
 """Static Institution Info Functions"""
 
+import time
 from dataclasses import dataclass
+from functools import lru_cache
 from typing import Any, Dict, cast
 
+from ..config import MAX_CACHE_MINS
 from .utils import mou_request
 
 
@@ -17,10 +20,21 @@ class Institution:
     institution_lead_uid: str
 
 
-def get_institutions_infos() -> Dict[str, Institution]:
-    """Get a dict of all institutions with their info."""
+@lru_cache()
+def _cached_get_institutions_infos(timeframe: int) -> Dict[str, Institution]:
+    """Cache is keyed by an int.
+
+    The int is used to auto-expire/regenerate cache results.
+    """
+    # pylint:disable=unused-argument
     resp = cast(Dict[str, Dict[str, Any]], mou_request("GET", "/institution/today"))
 
-    infos = {k: Institution(**v) for k, v in resp.items()}
+    return {k: Institution(**v) for k, v in resp.items()}
 
-    return infos
+
+def get_institutions_infos() -> Dict[str, Institution]:
+    """Get a dict of all institutions with their info."""
+
+    return _cached_get_institutions_infos(
+        time.time() // (60 * MAX_CACHE_MINS),  # make cache hit expire after X mins
+    )
