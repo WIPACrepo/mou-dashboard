fdc975923e09e0e25193bcd8b11a31a2724abcac
Ric Evans <emejqz@gmail.com>
2022-08-31 15:12:09 -0500

    add missing import

--- web_app/contents/__init__.py
+++ web_app/contents/__init__.py
@@ -1,6 +1,9 @@
 """Init."""
 
+# Everything imported here will get bounded to components
+
 from . import (  # noqa: F401
+    institution_callbacks,
     wbs_generic_admin_callbacks,
     wbs_generic_callbacks,
     wbs_generic_layout,
