b4f6c010223e972f874da0f13d9d8768e134ff95
Ric Evans <emejqz@gmail.com>
2022-10-21 16:53:36 -0500

    bypass user access token in CI

--- web_app/config.py
+++ web_app/config.py
@@ -31,6 +31,7 @@ class ConfigVarsTypedDict(TypedDict):
     FLASK_SECRET: str
     OIDC_CLIENT_SECRETS: str
     OVERWRITE_REDIRECT_URI: str
+    CI_TEST_ENV: bool
 
 
 def get_config_vars() -> ConfigVarsTypedDict:
@@ -44,6 +45,7 @@ def get_config_vars() -> ConfigVarsTypedDict:
             "FLASK_SECRET": "super-secret-flask-key",
             "OIDC_CLIENT_SECRETS": "client_secrets.json",
             "OVERWRITE_REDIRECT_URI": "",
+            "CI_TEST_ENV": False,
         }
     )
 
