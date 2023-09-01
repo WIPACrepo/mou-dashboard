5fd9feed594916c9c721d055d0c29cf95054cca2
Ric Evans <ric@evans-jacquez.com>
2023-09-01 14:40:39 -0500

    use `OPENID_URL` and `OPENID_URL`

--- rest_server/config.py
+++ rest_server/config.py
@@ -7,21 +7,24 @@ from wipac_dev_tools import from_environment_as_dataclass
 # --------------------------------------------------------------------------------------
 # Constants
 
+# pylint:disable=invalid-name
+
 
 @dc.dataclass(frozen=True)
 class EnvConfig:
     """Environment variables."""
 
-    # pylint:disable=invalid-name
-    MOU_AUTH_ALGORITHM: str = "HS512"  # 'RS256',
-    MOU_AUTH_ISSUER: str = "http://localhost:8888"  # 'MOUdash',
-    MOU_AUTH_SECRET: str = "secret"
+    OPENID_URL: str = "http://localhost:8888"
+    OPENID_AUDIENCE: str = "mou"
+
     MOU_MONGODB_AUTH_USER: str = ""  # None means required to specify
     MOU_MONGODB_AUTH_PASS: str = ""  # empty means no authentication required
     MOU_MONGODB_HOST: str = "localhost"
     MOU_MONGODB_PORT: int = 27017
+
     MOU_REST_HOST: str = "localhost"
     MOU_REST_PORT: int = 8080
+
     CI_TEST: bool = False
 
 
