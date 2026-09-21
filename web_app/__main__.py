88ab2101a850609375ff3ed6f1b26f31519a3524
David Schultz <david.schultz@icecube.wisc.edu>
2026-09-21 12:33:16 -0700

    Revert to the same major version of dash as was used previously (#92)
    * go back to 2.x dash to hopefully fix compatibility
    
    * Revert "contain the exception on redirect (#90)"
    
    This reverts commit fcd06534122d1eb081b995f521242fa7e9d86478.
    
    * fix dependencies

--- web_app/__main__.py
+++ web_app/__main__.py
@@ -19,7 +19,7 @@ def main() -> None:
     layout.layout()
 
     # Run Server
-    app.run(
+    app.run_server(
         debug=ENV.DEBUG,
         host=ENV.WEB_SERVER_HOST,
         port=ENV.WEB_SERVER_PORT,
